1d8b24de6e7eeba16119b2e9bb4745824ee016b6
caefleury <caefleury15@gmail.com>
2024-01-06 02:34:43 -0300

    feat: iterate html table and convert to list of dictionaries

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import json
 from utils import return_depto_by_name
@@ -40,17 +43,17 @@ select_nivel = Select(select_element_nivel)
 select_nivel.select_by_value('G')
 
 # Selecione o departamento 
-depto = depto_data[30]['value'] # 30 = Computação
+depto = depto_data[17]['value'] # 30 = Computação  , 17 = Matemática
 select_depto = Select(select_element_depto)
 select_depto.select_by_value(depto)
 
 # Seleciona o ano
-ANO = '2024'
+ANO = '2023'
 input_element_ano.clear() 
 input_element_ano.send_keys(ANO)
 
 # Selecione o periodo
-PERIODO = '1'
+PERIODO = '4'
 select_periodo = Select(select_element_periodo)
 select_periodo.select_by_value(PERIODO)
 
@@ -67,6 +70,35 @@ except NoSuchElementException as e:
 
 # Mostra o codigo HTML da página no terminal
 # print(driver.page_source)
+        
+classes = []
+count = 1
+turma = {}
+
+while True:
+    try:
+        tr = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]")
+    except NoSuchElementException:
+        break
+    if tr.get_attribute('class') == 'agrupador': 
+                classes.append(turma)
+                turma = {}
+                name = tr.find_element(By.CLASS_NAME, "tituloDisciplina").text
+                class_name = name.split(' - ', 1)[1]
+                class_code = name.split()[0]
+                turma['class_name'] = class_name
+                turma['class_code'] = class_code
+    else:
+        turma['class_number'] = tr.find_element(By.CLASS_NAME, "turma").text
+        turma['anoPeriodo'] = tr.find_element(By.CLASS_NAME, "anoPeriodo").text
+        turma['professor'] = tr.find_element(By.CLASS_NAME, "nome").text
+        turma['horario'] = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]/td[4]").text
+        turma['vagas_ofertadas'] = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]/td[6]").text
+        turma['vagas_ocupadas'] = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]/td[7]").text
+        turma['local'] = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]/td[8]").text
+
+    count += 1
+
 
 # Impede que o navegador feche para fins de teste
 while(True):
