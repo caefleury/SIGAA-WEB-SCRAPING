6b9353f6812aa6b9ca5e3501535c6e7693f74d7e
caefleury <caefleury15@gmail.com>
2024-01-08 12:13:39 -0300

    refactor: move functions to utils.py for code organization

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import json
-from utils import return_depto_by_name
+from utils import fill_select,fill_input,form_submit,button_click
 
 # Dados do departamento
 with open('./data/depto_data.json', 'r') as json_file:
@@ -16,6 +16,12 @@ with open('./data/depto_data.json', 'r') as json_file:
 # Defina a URL que você quer acessar
 URL ='https://sigaa.unb.br/sigaa/public/turmas/listar.jsf'
 
+# Defia os inputs da busca
+nivel = 'G'
+depto = depto_data[17]['value'] # 30 = Computação  , 17 = Matemática
+ANO = '2024'
+PERIODO = '1'
+
 # Inicie o navegador
 driver = webdriver.Chrome() # ou webdriver.Firefox(), dependendo do seu navegador
 
@@ -23,50 +29,14 @@ driver = webdriver.Chrome() # ou webdriver.Firefox(), dependendo do seu navegado
 driver.get(URL)
 print(json)
 
-try:
-    select_element_nivel = driver.find_element('id', 'formTurma:inputNivel')
-    select_element_depto = driver.find_element('id', 'formTurma:inputDepto')
-    input_element_ano = driver.find_element('id', 'formTurma:inputAno')
-    select_element_periodo = driver.find_element('id', 'formTurma:inputPeriodo')
-except NoSuchElementException as e:
-    if 'formTurma:inputNivel' in str(e):
-        print("Nivel element not found")
-    elif 'formTurma:inputDepto' in str(e):
-        print("Depto element not found")
-    elif 'formTurma:inputAno' in str(e):
-        print("Ano element not found")
-    elif 'formTurma:inputPeriodo' in str(e):
-        print("Periodo element not found")
-
-# Selecione o nivel com value='G'
-select_nivel = Select(select_element_nivel)
-select_nivel.select_by_value('G')
-
-# Selecione o departamento 
-depto = depto_data[17]['value'] # 30 = Computação  , 17 = Matemática
-select_depto = Select(select_element_depto)
-select_depto.select_by_value(depto)
-
-# Seleciona o ano
-ANO = '2023'
-input_element_ano.clear() 
-input_element_ano.send_keys(ANO)
-
-# Selecione o periodo
-PERIODO = '4'
-select_periodo = Select(select_element_periodo)
-select_periodo.select_by_value(PERIODO)
+fill_select(driver,'id', 'formTurma:inputNivel', nivel)
+fill_select(driver,'id', 'formTurma:inputDepto', depto)
+fill_input(driver,'id', 'formTurma:inputAno', ANO)
+fill_select(driver,'id', 'formTurma:inputPeriodo', PERIODO)
 
 # Submeta o formulário
-form_element = driver.find_element('name', 'formTurma')
-form_element.submit()
-
-try:
-    submit_button = driver.find_element('name', 'formTurma:j_id_jsp_1370969402_11')
-    submit_button.click()
-except NoSuchElementException as e:
-    if 'formTurma:j_id_jsp_1370969402_11' in str(e):
-        print("Submit element not found")
+form_submit(driver,'id', 'formTurma')
+button_click(driver,'name', 'formTurma:j_id_jsp_1370969402_11')
 
 # Mostra o codigo HTML da página no terminal
 # print(driver.page_source)
@@ -74,7 +44,6 @@ except NoSuchElementException as e:
 classes = []
 count = 1
 turma = {}
-
 while True:
     try:
         tr = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]")
@@ -98,7 +67,6 @@ while True:
         turma['local'] = driver.find_element(By.XPATH, f"//div[@id='turmasAbertas']/table/tbody/tr[{count}]/td[8]").text
 
     count += 1
-
 
 # Impede que o navegador feche para fins de teste
 while(True):
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Retorna as infromações do departamento atraves do nome
-def return_depto_by_name(depto_data, depto_name):
-    for depto in depto_data:
-        if depto.get('name') == depto_name:
-            return depto
-    return None
+# Encontra o select e o preenche com os valores
+def fill_select(driver,tag,atribute_value,select_value):
+    try:
+        select_element = driver.find_element(tag, atribute_value)
+        select = Select(select_element)
+        select.select_by_value(select_value)
+    except NoSuchElementException as e:
+        return e
+
+def fill_input(driver,tag,atribute_value,input_value):
+    try:
+        input_element = driver.find_element(tag, atribute_value)
+        input_element.clear() 
+        input_element.send_keys(input_value)
+    except NoSuchElementException as e:
+        return e
+
+def form_submit(driver,tag,atribute_value):
+    try:
+        form_element = driver.find_element(tag, atribute_value)
+        form_element.submit()
+    except NoSuchElementException as e:
+        return e
+    
+def button_click(driver,tag,atribute_value):
+    try:
+        button_element = driver.find_element(tag, atribute_value)
+        button_element.click()
+    except NoSuchElementException as e:
+        return e
